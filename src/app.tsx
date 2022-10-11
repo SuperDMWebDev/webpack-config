8aedbbff3ea9a792c5f8203cf897e10dcd9da747
DMSofwareDev <89517604+SuperDMWebDev@users.noreply.github.com>
2022-10-11 14:53:59 +0700

    no responsive

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "@/container/app";
-import  "./style.scss";
+import "./style.scss";
+import "./reset.css";
+import "./style.css";
 
 const container = document.getElementById("app") as HTMLElement;
 const root = createRoot(container);
@@ -12,6 +14,7 @@ root.render(
   </BrowserRouter>
 );
 
-if (module?.['hot']) { // enables hot module replacement if plugin is installed
-  module?.['hot'].accept();
+if (module?.["hot"]) {
+  // enables hot module replacement if plugin is installed
+  module?.["hot"].accept();
 }
